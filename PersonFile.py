fdd003504c400ac09c53c6fcb7dd690e22072a5a
Brian Evans <ebrian101@gmail.com>
2020-03-10 22:00:28 +0000

    Added faster simulation without animation
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- PersonFile.py
+++ PersonFile.py
@@ -4,17 +4,17 @@ import random
 class Person:
     population = 0
 
-    def __init__(self, floors):
+    def __init__(self, floors, floorheight):
         self.animation = None
+        self.id = Person.population
         Person.population += 1
-
         self.start_floor = random.randint(0, floors)
         self.target_floor = random.randint(0, floors)
         while self.start_floor == self.target_floor:
             self.target_floor = random.randint(0, floors)
         self.direction = ('up' if self.start_floor < self.target_floor else 'down')
-
+        self.distance = floorheight*(self.start_floor-self.target_floor)
         self.finished = False
-
+        self.in_elevator = False
     def arrived(self, floor):
         return True if floor == self.target_floor else False
